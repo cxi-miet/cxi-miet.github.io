097a3f70d513edab4971118f724ff11978cd253f
isha988 <ishakapoor.988@gmail.com>
2022-11-17 19:24:52 +0530

    main2.js added

--- js/eventdata.js
+++ js/eventdata.js
@@ -16,5 +16,77 @@ export const events = [
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
         registration: "2022-09-11",
         link: "https://www.google.com"  
+    },
+    {
+        name: "event 1",
+        date: "2022-12-20",
+        mode: "offline",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-12-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 2",
+        date: "2022-10-20",
+        mode: "online",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-09-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 1",
+        date: "2022-12-20",
+        mode: "offline",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-12-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 2",
+        date: "2022-10-20",
+        mode: "online",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-09-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 1",
+        date: "2022-12-20",
+        mode: "offline",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-12-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 2",
+        date: "2022-10-20",
+        mode: "online",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-09-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 1",
+        date: "2022-12-20",
+        mode: "offline",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-12-11",
+        link: "https://www.google.com"  
+    },
+    {
+        name: "event 2",
+        date: "2022-10-20",
+        mode: "online",
+        prize: "5000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet? Voluptatum iusto esse nihil deserunt facilis porro quidem minus optio ipsa, natus cumque impedit repellendus maiores aperiam rerum quaerat illo?",
+        registration: "2022-09-11",
+        link: "https://www.google.com"  
     }
 ]
