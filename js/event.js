04b67a4ecd6da7c8bd341f84da86058697a5f33f
Isha Kapoor <89572340+Isha988@users.noreply.github.com>
2022-11-20 18:28:22 +0530

    Event Page Added (#19)
    * event page completed
    
    * main2.js added

---
+++ js/event.js
@@ -0,0 +1,42 @@
+import {events} from "./eventdata.js";
+
+const upcomingEventRow = document.querySelector("#upcoming-event-cover");
+const pastEventRow = document.querySelector("#past-event-cover");
+
+const upcomingEventTemp = document.querySelector("#up-event-temp");
+const pastEventTemp = document.querySelector("#past-event-temp");
+
+const date = new Date();
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+window.addEventListener("load", createEvent);
+
+function createEvent(){
+    events.map((evt) => {
+        const evtDate = new Date(evt.date);
+        if(evtDate > date) {
+            const upcomingEvt = upcomingEventTemp.content.cloneNode(true);
+            entries(upcomingEvt, evt, evtDate);
+            upcomingEvt.querySelector(".reg-date").textContent = new Date(evt.registration).toLocaleDateString();
+            upcomingEventRow.append(upcomingEvt);
+        }
+        else {
+            const pastEvt = pastEventTemp.content.cloneNode(true);
+            entries(pastEvt, evt, evtDate);
+            pastEventRow.append(pastEvt);
+        }
+    }) 
+}
+
+function entries(clone, evt, evtDate){
+    clone.querySelector(".day").textContent = evtDate.getDate();
+    clone.querySelector(".month").textContent = months[evtDate.getMonth()];
+    clone.querySelector(".event-name").textContent = evt.name;
+    clone.querySelector(".mode").textContent = evt.mode;
+    clone.querySelector(".prize").textContent = evt.prize;
+    clone.querySelector(".event-desc").textContent = evt.description;
+    clone.querySelector(".link").href = evt.link;  
+}
+
---
+++ js/main2.js
@@ -0,0 +1,68 @@
+jQuery(function($) {
+    //Preloader
+	var preloader = $('.preloader');
+	$(window).load(function(){
+		preloader.remove();
+	});
+
+	//#main-slider
+	var slideHeight = $(window).height();
+	$('#home-slider .item').css('height',slideHeight);
+
+	$(window).resize(function(){
+        'use strict';
+		$('#home-slider .item').css('height',slideHeight);
+	});
+	
+	//Scroll Menu
+	$(window).on('scroll', function(){
+		if( $(window).scrollTop()>slideHeight ){
+			$('.main-nav').addClass('navbar-fixed-top');
+		} else {
+			$('.main-nav').removeClass('navbar-fixed-top');
+		}
+	});
+	
+	// Navigation Scroll
+	$(window).scroll(function(event) {
+		Scroll();
+	});
+
+	$('.navbar-collapse ul li a').on('click', function() {  
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+        $('.navbar-collapse.collapse.in').removeClass('in');
+		return false;
+	});
+	// User define function
+	function Scroll() {
+		var contentTop      =   [];
+		var contentBottom   =   [];
+		var winTop      =   $(window).scrollTop();
+		var rangeTop    =   200;
+		var rangeBottom =   500;
+		$('.navbar-collapse').find('.scroll a').each(function(){
+			contentTop.push( $( $(this).attr('href') ).offset().top);
+			contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+		})
+		$.each( contentTop, function(i){
+			if ( winTop > contentTop[i] - rangeTop ){
+				$('.navbar-collapse li.scroll')
+				.removeClass('active')
+				.eq(i).addClass('active');			
+			}
+		})
+	};
+
+	$('#tohash').on('click', function(){
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		return false;
+	});
+    
+    if($(window).width() > 767){
+        //Initiat WOW JS
+        new WOW().init();
+    }
+	
+	//smoothScroll
+	smoothScroll.init();
+})
